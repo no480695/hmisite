c54147faea72e31bd58eeae0a53277c6d53e0910
Nathan <nathan@sterling.(none)>
2013-05-15 11:34:51 -0400

    fix date input parsing for events

--- pages/era/save-new-event.php
+++ pages/era/save-new-event.php
@@ -1,8 +1,8 @@
 <?
 
 $era_id 		= $_POST['era_id'];
-$start_date 	= "'".$_POST['start_date']."'";
-$end_date 		= "'".$_POST['end_date']."'";
+$start_date 	= "'".convertDate($_POST['start_date'])."'";
+$end_date 		= "'".convertDate($_POST['end_date'])."'";
 
 if ( $end_date == "''" ){ $end_date = $start_date; }
 
@@ -10,6 +10,7 @@ $name 			= "'".str_replace("'","`",$_POST['name'])."'";
 $location 		= "'".str_replace("'","`",$_POST['location'])."'";
 $description 	= "'".str_replace("'","`",$_POST['description'])."'";
 $mod_person_id 	= $_POST['mod_person_id'];
+
 
 
 $dbh=mysql_connect ("localhost", "noffutt", "beaver12")
@@ -21,6 +22,20 @@ if ( mysql_query("INSERT INTO event (era_id,start_date,end_date,name,location,de
 }
 else {
 	exit ("INSERT INTO event (era_id,start_date,end_date,name,location,description,mod_person_id) VALUES (".$era_id.",".$start_date.",".$end_date.",".$name.",".$location.",".$description.",".$mod_person_id.")");
+}
+
+
+function convertDate($input_date){
+
+	//08/12/2013 12:00
+	//YYYY-MM-DD HH:mm:SS
+	$parts = explode(" ",$input_date);
+	$date_parts  = explode("/",$parts[0]);
+	$time_parts = explode(":",$parts[1]);
+
+	$output_date = $date_parts[2]."-".$date_parts[0]."-".$date_parts[1]." ".$time_parts[0].":".$time_parts[1];
+
+ return $output_date;
 }
 
 ?>
