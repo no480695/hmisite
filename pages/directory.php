ddc0ffb5dc65a54c9ed1b666faaf362ff1130592
Nathan <nathan@sterling.(none)>
2013-05-16 14:24:08 -0400

    clear for blank canvas and also set header for directory

--- pages/directory.php
+++ pages/directory.php
@@ -21,7 +21,7 @@ if ( !$_SESSION['user_name'] ){
     <link href="/css/bootstrap-responsive.css" rel="stylesheet">
 	<link href="/css/jquery-ui-1.8.21.custom.css" />
 	<link href="/css/fcal.css" rel="stylesheet">
-	
+
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -30,7 +30,7 @@ if ( !$_SESSION['user_name'] ){
   </head>
 
   <body>
-  
+
 	<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 	  <div class="modal-header">
 		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
@@ -43,33 +43,38 @@ if ( !$_SESSION['user_name'] ){
 		<button id="submit-form" class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
 	  </div>
 	</div>
-  
-  
+
+
 
     <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
-          
-          <a class="brand" href="">Historical Military Impressions</a>
+
+          <a class="brand" href="/">Historical Military Impressions</a>
           <div class="nav-collapse collapse">
             <ul class="nav">
-              <li><a href="">Home</a></li>
-              <li><a href="/pages/ladies.php">Ladies of HMI</a></li>
+              <li><a href="/">Home</a></li>
               <li><a href="/pages/application.php">Membership Application</a></li>
-			  <li><a href="/pages/newsletter.php">Newsletter</a></li>
-			  <li class="active"><a href="/pages/directory.php">Directory</a></li>
-              <!-- Read about Bootstrap dropdowns at http://twitter.github.com/bootstrap/javascript.html#dropdowns -->
+
+              <li class="dropdown active">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Members <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="/pages/ladies.php">Ladies of HMI</a></li>
+                  <li><a href="/pages/merit-award.php">HMI Merit Award Winners</a></li>
+                  <li><a href="/pages/newsletter.php">Newsletter</a></li>
+                  <li class="active"><a href="/pages/directory.php">Directory</a></li>
+                </ul>
+              </li>
+
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Impressions <b class="caret"></b></a>
                 <ul class="dropdown-menu">
                   <li><a href="/pages/era/wwii.php">World War Two</a></li>
                   <li><a href="/pages/era/wwi.php">World War One</a></li>
                   <li><a href="/pages/era/civilwar.php">Civil War</a></li>
-                  <!--<li class="divider"></li>
-                  <li class="nav-header">Nav header</li>-->
                   <li><a href="/pages/era/1812.php">War of 1812</a></li>
                   <li><a href="/pages/era/revwar.php">Rev War</a></li>
-				  <li><a href="/pages/era/french&indian.php">French and Indian War</a></li>
+                  <li><a href="/pages/era/french&indian.php">French and Indian War</a></li>
                 </ul>
               </li>
 			  <? if ( $_SESSION['user_name'] ){ ?>
@@ -88,18 +93,18 @@ if ( !$_SESSION['user_name'] ){
       </div>
     </div>
 
-	
-	
+
+
     <div class="container">
 			<h2>Membership Directory</h2>
 			<?
-	
+
 				include("process.php");
-	
-			?>	
-		
+
+			?>
+
     </div> <!-- /container -->
-	
+
 	<footer class="footer">
 		<div class="container">
 			<p class="pull-right"><a href="#">Back to top</a></p>
@@ -122,7 +127,7 @@ if ( !$_SESSION['user_name'] ){
 					$('#thanks-message').fadeIn('fast');
 				});
 			});
-			
+
 			$('#submit-info').click(function(){
 				alert('now send to ajax call');
 			});
